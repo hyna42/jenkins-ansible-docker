49f6c9780a779def8e13e626d35c7311161edce0
hyna42 <bylo@duck.com>
2026-07-25 21:15:26 +0200

    fix: generate sshkey

--- copyssh.sh
+++ copyssh.sh
@@ -1,7 +1,16 @@
 # Objectif : copier la clé ssh publique de la machine jenkins vers la machine ubuntu-target
 
-# 1. Générer la clé SSH sur le driver jenkins
-docker exec -u root jenkins /bin/bash -c \ "ssh-keygen -t ed25519 -C 'jenkins@driver' -f ~/.ssh/id_ed25519 -N ''"
+# 1. Suppression de l'ancienne clé si elle existe
+docker exec -u root jenkins /bin/bash -c "rm -f /var/jenkins_home/.ssh/id_ed25519*"
 
-# 2. Copier la clé publique id_ed25519.pub jenkins vers le target ubuntu-target
-sshpass -p "test" ssh-copy-id -i /home/hyna/.ssh/id_ed25519.pub root@172.19.0.11
+# 2. Générer la clé SSH sur le driver jenkins
+docker exec jenkins /bin/bash -c "ssh-keygen -t ed25519 -C 'jenkins@driver' -f /var/jenkins_home/.ssh/id_ed25519 -N ''"
+
+# 3. Extraire la clé publique du conteneur vers un fichier local sur ubuntu-lab
+docker exec jenkins bash -c "cat /var/jenkins_home/.ssh/id_ed25519.pub" > jenkins_id_ed25519.pub
+
+# 4. Copier la clé du conteneur jenkins vers le target ubuntu-target
+cat jenkins_id_ed25519.pub | sshpass -p "test" ssh -o StrictHostKeyChecking=no root@172.19.0.11 "mkdir -p /root/.ssh && cat >> /root/.ssh/authorized_keys"
+
+# 5 Configuration des permissions sur ubuntu-target
+sshpass -p "test" ssh -o StrictHostKeyChecking=no root@172.19.0.11 "chmod 700 /root/.ssh && chmod 600 /root/.ssh/authorized_keys"
